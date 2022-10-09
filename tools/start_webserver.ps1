6e1d871cdb2d4a6d71fc2cf91a49780fe51de819
Rui Cavaco <rpcavaco@gmail.com>
2022-10-09 16:59:18 +0100

    Still completing moving tools to their folder

--- tools/start_webserver.ps1
+++ tools/start_webserver.ps1
@@ -1,11 +1,15 @@
 
 # Atualiza conteudo do site de teste e inicia webserver local
 
-Remove-Item -Path "dist\*.js"
+Remove-Item -Path "..\dist\riscojs_v2\*.mjs"
+Remove-Item -Path "..\dist\riscojs_v2\*.js"
 
-Copy-Item -Path "src\*.js" -Destination "dist\riscojs_v2"
-Copy-Item -Path "src\*.js" -Destination "testbed\js\riscojs_v2"
+Remove-Item -Path "..\testbed\js\riscojs_v2\*.mjs"
+Remove-Item -Path "..\testbed\js\riscojs_v2\*.js"
 
-Compress-Archive -Path "dist\riscojs_v2" -DestinationPath "riscojs_v2.zip" -Force
+Copy-Item -Path "..\src\*" -Include "*.js","*.mjs" -Destination "..\dist\riscojs_v2"
+Copy-Item -Path "..\src\*" -Include "*.js","*.mjs" -Destination "..\testbed\js\riscojs_v2"
 
-python -m http.server --directory testbed 9000
+Compress-Archive -Path "..\dist\riscojs_v2" -DestinationPath "..\riscojs_v2.zip" -Force
+
+python -m http.server --directory ..\testbed 9000
