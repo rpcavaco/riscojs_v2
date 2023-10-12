20d69e75036ce4f9ef6aecca285f2e8cc6641fca
Rui Cavaco <rpcavaco@gmail.com>
2023-10-12 13:20:54 +0100

    Small change in HOMESYMB (constants.js)

--- src/constants.js
+++ src/constants.js
@@ -191,7 +191,7 @@ var GlobalConst = {
 		COLORDISABLED: "rgb(80, 80, 80)", // Still not in use
 		OFFSET: 10,
 		SIZE: 30,
-		STROKEWIDTH: 1,
+		STROKEWIDTH: 1.2,
 		GAP: 10,
 		MOBILE_DEFGAP: 5,
 		FONTFAMILY: "OpenSans-CondensedRegular",
@@ -202,7 +202,7 @@ var GlobalConst = {
 		DROPDOWNARROWSZ: 6,
 
 		HOMESYMBWID: 24,
-		HOMESYMB: "data:image/svg+xml;charset=utf-8,%3Csvg width=%2224%22 height=%2224%22 viewBox=%220 0 24 24%22 xmlns=%22http://www.w3.org/2000/svg%22%3E %3Cpath fill=%22none%22 stroke=%22black%22 stroke-width=%221%22 stroke-linejoin=%22round%22 d=%22M 4,14 L 12,8 L 20,14 h -3 v 8 h -3 v -4 h -4 v 4 h -3 v -8 z %22 /%3E%3C/svg%3E",
+		HOMESYMB: "data:image/svg+xml;charset=utf-8,%3Csvg width=%2224%22 height=%2224%22 viewBox=%220 0 24 24%22 xmlns=%22http://www.w3.org/2000/svg%22%3E %3Cpath fill=%22none%22 stroke=%22black%22 stroke-width=%221.2%22 stroke-linejoin=%22round%22 d=%22M 4,14 L 12,8 L 20,14 h -3 v 8 h -3 v -4 h -4 v 4 h -3 v -8 z %22 /%3E%3C/svg%3E",
 
 		TOC_LEFTCOL_WIDTH: 50,
 		TOC_VARSTYLESZ_PX: 14,
